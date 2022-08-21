62b96b7da71c49853421437a7221f8974e474d17
jjaegii <hn06038@gmail.com>
2022-08-21 13:53:56 +0000

    django projects

---
+++ Ncheat/urls.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.views.generic import TemplateView
+from user.views import CustomPasswordChangeView
+
+urlpatterns = [
+    # admin
+    path('admin/', admin.site.urls),
+    # user
+    path('', include('user.urls')),
+
+    path('email-confirmation-done/',
+         TemplateView.as_view(
+             template_name="account/email_cofirmation_done.html"),
+         name="account_email_confirmation_done"
+         ),
+    path('password/change/',
+         CustomPasswordChangeView.as_view(),  # allauth에 되기전에 먼저?
+         name="account_password_change"
+         ),
+    path('', include('allauth.urls')),
+
+    path('meeting/', include('meeting.urls'))
+]
