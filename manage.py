4ea3847877b7a91b15bcaa2c436ca4a0f5ee43ad
jjaegii <hn06038@gmail.com>
2022-08-24 00:08:32 +0900

    change project name

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Ncheat.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'catchcunning.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
